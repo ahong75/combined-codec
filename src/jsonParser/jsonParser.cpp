45fb58a038de661f5abbb607c71b45c33bd51ec8
Anthony Hong <ahong75@gatech.edu>
2022-06-03 13:11:42 -0400

    structure changes

--- src/jsonParser/jsonParser.cpp
+++ src/jsonParser/jsonParser.cpp
@@ -1,59 +1,62 @@
-#include "../lib/json.hpp"
-#include "../include/jsonParser.hpp"
-#include <iostream>
+#include "../../include/jsonParser.hpp"
+#include "../../lib/json.hpp"
 #include <fstream>
+#include <iostream>
 
 using namespace std;
 using json = nlohmann::json;
 
 void jsonParser::parseJSONFile(string file) {
-    ifstream jsonFile(file);
-    j = json::parse(jsonFile);
-    setParameters();
+  ifstream jsonFile(file);
+  j = json::parse(jsonFile);
+  setParameters();
 }
 void jsonParser::insertError() {
-    jsonParser::insertErrorRate = (double)j["simulation"]["error"]["insert"];
+  jsonParser::insertErrorRate = (double)j["simulation"]["error"]["insert"];
 }
 void jsonParser::deleteError() {
-    jsonParser::deleteErrorRate = (double)j["simulation"]["error"]["delete"];
+  jsonParser::deleteErrorRate = (double)j["simulation"]["error"]["delete"];
 }
 void jsonParser::substitutionError() {
-    jsonParser::substitutionErrorRate = (double)j["simulation"]["error"]["substitution"];
+  jsonParser::substitutionErrorRate =
+      (double)j["simulation"]["error"]["substitution"];
 }
 void jsonParser::numReads() {
-    jsonParser::numberReads = (int)j["simulation"]["oligos"]["numReads"];
+  jsonParser::numberReads = (int)j["simulation"]["oligos"]["numReads"];
 }
 void jsonParser::numOligos() {
-    jsonParser::numberOligos = (int)j["simulation"]["oligos"]["numOligos"];
+  jsonParser::numberOligos = (int)j["simulation"]["oligos"]["numOligos"];
 }
 void jsonParser::lenOligos() {
-    jsonParser::lengthOligos = (int)j["simulation"]["oligos"]["oligoLength"];
+  jsonParser::lengthOligos = (int)j["simulation"]["oligos"]["oligoLength"];
 }
 void jsonParser::inputFile() {
-    jsonParser::inputFileName = j["simulation"]["input"]["inputFileName"];
+  jsonParser::inputFileName = j["simulation"]["input"]["inputFileName"];
 }
 void jsonParser::randInput() {
-    jsonParser::randomInput = j["simulation"]["input"]["randomInput"];
+  jsonParser::randomInput = j["simulation"]["input"]["randomInput"];
 }
 void jsonParser::unorderedOutputFile() {
-    jsonParser::unorderedOutput = j["simulation"]["output"]["unorderedOutputFileName"];
+  jsonParser::unorderedOutput =
+      j["simulation"]["output"]["unorderedOutputFileName"];
 }
 void jsonParser::orderedOutputFile() {
-    jsonParser::orderedOutput = j["simulation"]["output"]["orderedOutputFileName"];
+  jsonParser::orderedOutput =
+      j["simulation"]["output"]["orderedOutputFileName"];
 }
 void jsonParser::numSamples() {
-    jsonParser::numberSamples = (int)j["simulation"]["output"]["numSamples"];
+  jsonParser::numberSamples = (int)j["simulation"]["output"]["numSamples"];
 }
 void jsonParser::setParameters() {
-    insertError();
-    deleteError();
-    substitutionError();
-    numReads();
-    numOligos();
-    lenOligos();
-    inputFile();
-    unorderedOutputFile();
-    orderedOutputFile();
-    randInput();
-    numSamples();
+  insertError();
+  deleteError();
+  substitutionError();
+  numReads();
+  numOligos();
+  lenOligos();
+  inputFile();
+  unorderedOutputFile();
+  orderedOutputFile();
+  randInput();
+  numSamples();
 }
---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+#include "../include/codec.hpp"
+#include "../include/jsonParser.hpp"
+#include <fstream>
+#include <iostream>
+#include <random>
+#include <string>
+#include <vector>
+
+std::random_device dev;
+std::mt19937 rng(dev());
+std::uniform_int_distribution<std::mt19937::result_type> dist1(1, 255);
+
+// Simply tests if the codec can encode and decode the original file when there
+// are zero errors
+void test(int width, int height) {
+  std::vector<unsigned char> orig(width * height);
+  for (int i = 0; i < width * height; i++) {
+    orig[i] = i;
+  }
+  // encode(orig, width, height);
+  std::vector<unsigned char> decoded = decode("encoded.fasta", width, height);
+  if (orig == decoded) {
+    std::cout << "Passed" << std::endl;
+  } else {
+    std::cout << "Failed" << std::endl;
+  }
+}
+
+int main() {
+  // What needs to be tested:
+  // Whether the program can encode and decode with 0 errors
+  // Simply pass in the outputted fasta
+  // Whether it can recover from different types of errors
+
+  for (int i = 0; i < 100; i++) {
+    test(10, 12);
+  }
+}
